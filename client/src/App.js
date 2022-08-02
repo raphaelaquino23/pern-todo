8ea43e350d867d6f97aabbdf5d81302df8a752fa
Raphael <raphaelaquino23@gmail.com>
2022-08-02 17:16:11 +0800

    finish edit (but modal is outdated / tut ended)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Fragment } from 'react';
 import './App.css';
 
-import EditTodo from './components/EditTodo';
 import InputTodo from './components/InputTodo';
 import ListTodos from './components/ListTodos';
 
--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,72 +1,71 @@
 import React, { Fragment, useEffect, useState } from "react";
 
+import EditTodo from "./EditTodo";
+
 const ListTodos = () => {
   const [todos, setTodos] = useState([]);
 
-  const deleteTodo = async(id) => {
-    try{
-      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
-        method: 'DELETE'
-      });
-      setTodos(todos.filter(todo => todo.todo_id !== id));
-    } catch (err){
-      console.error(err.message);
-    }
-  }
+  //delete todo function
 
-  const editTodo = async(id) => {
-    try{
-      const editTodo = await fetch(`http://localhost:5000/todos/${id}`, {
-        method: 'EDIT'
-      });
-      setTodos(todos.filter(todo => todo.todo_id !== id));
-    } catch (err){
-      console.error(err.message);
-    }
-  }
-
-  const getTodos = async() => {
+  const deleteTodo = async id => {
     try {
-      const response = await fetch('http://localhost:5000/todos');
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE"
+      });
+
+      setTodos(todos.filter(todo => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/todos");
       const jsonData = await response.json();
 
       setTodos(jsonData);
-    } catch (err){
+    } catch (err) {
       console.error(err.message);
     }
-  }
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getTodos();
   }, []);
 
+  console.log(todos);
+
   return (
     <Fragment>
-      <div class="container mt-5">
-        <h2>Basic Table</h2>
-        <table class="table ">
-          <thead>
-            <tr>
-              <th>Description</th>
-              <th>Edit</th>
-              <th>Delete</th>
+      {" "}
+      <table class="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map(todo => (
+            <tr key={todo.todo_id}>
+              <td>{todo.description}</td>
+              <td>
+                <EditTodo todo={todo} />
+              </td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {todos.map(todo => (
-              <tr key ={todo.todo_id}>
-                <td>{todo.description}</td>
-                <td>
-                  <button className="btn btn-warning" onClick={()=>editTodo(todo.todo_id)}>Edit</button>
-                  </td>
-                <td>
-                  <button className="btn btn-danger" onClick={()=> deleteTodo(todo.todo_id)}>Delete</button>
-                  </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
     </Fragment>
   );
 };
