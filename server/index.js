90d360504180a762e33e2fdc740454eef783409f
Raphael <raphaelaquino23@gmail.com>
2022-08-02 14:46:57 +0800

    connect db to server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
 
 /* 
  * Anytime you make a full-stack app you need to get data from client side
@@ -11,6 +12,15 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json()); // will reference this a lot (request body object)
 
+// ROUTES //
+
+// Create a todo
+
+// Get all todos
+
+// Get a single todo
+
+// Delete a todo
 
 // opens a port on the specified location, in this case 5000
 app.listen(5000, () => {
