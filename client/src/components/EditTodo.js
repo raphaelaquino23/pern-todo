38dde55791d61439592cbacb423cbe85d6d28778
raphaelaquino23 <raphaelaquino23@gmail.com>
2022-08-04 16:51:55 +0800

    Revert "first commit using two projects or multiple projects demo"
    This reverts commit 25a93a7193b9981c1010c79027a5bc6175c7b821.

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -3,7 +3,7 @@ import React, { Fragment, useState } from "react";
 const EditTodo = ({ todo }) => {
   const [description, setDescription] = useState(todo.description);
 
-  const updateDescription = async (e) => {
+  const updateDescription = async e => {
     e.preventDefault();
     try {
       const body = { description };
@@ -12,7 +12,7 @@ const EditTodo = ({ todo }) => {
         {
           method: "PUT",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(body),
+          body: JSON.stringify(body)
         }
       );
 
@@ -32,6 +32,10 @@ const EditTodo = ({ todo }) => {
       >
         Edit
       </button>
+
+      {/* 
+        id = id10
+      */}
       <div
         class="modal"
         id={`id${todo.todo_id}`}
@@ -56,7 +60,7 @@ const EditTodo = ({ todo }) => {
                 type="text"
                 className="form-control"
                 value={description}
-                onChange={(e) => setDescription(e.target.value)}
+                onChange={e => setDescription(e.target.value)}
               />
             </div>
 
@@ -65,7 +69,7 @@ const EditTodo = ({ todo }) => {
                 type="button"
                 class="btn btn-warning"
                 data-dismiss="modal"
-                onClick={(e) => updateDescription(e)}
+                onClick={e => updateDescription(e)}
               >
                 Edit
               </button>
